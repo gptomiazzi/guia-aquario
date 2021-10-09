e09e41e682630f7ab6a93317d2ae945382e512d5
gabs <gabrieltomiazzi@outlook.com>
2021-10-08 21:36:20 -0300

    Teste do grid

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+import styled from "styled-components"
+
 export default function Home() {
   
+    const MainGrid = styled.main`
+        display: grid;
+        grid-template-areas: "sidebar content";
+        grid-template-columns: 15% 1fr;
+        grid-template-rows: 100vh;
+        grid-column-gap: 1px;
+        grid-row-gap: 0px;
+    `;
+
     return (
-        <p>olá</p>
+
+        <MainGrid>
+            <section className="teste">
+                <p style={{ gridArea: "sidebar" }}>aaaaaaaaaaaaaaaaaa</p>
+            </section>
+        </MainGrid>
     )
 }
